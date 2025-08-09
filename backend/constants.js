30e1882c65d775ee430f52b4e189dfd7bfa81edc
Akrati_singh <127114340+geekymee@users.noreply.github.com>
2025-08-09 10:55:02 +0530

    Update constants.js

--- backend/constants.js
+++ backend/constants.js
@@ -3,6 +3,7 @@ export const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:5173',
     'http://localhost:5173',
-    'seat-wise-api.vercel.app'
+    'https://seat-wise-api.vercel.app',
+    'https://seat-wise-frontend.vercel.app'
 ];
 
